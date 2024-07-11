2bce78faddc67d6e0488b2eac8594441880df5e2
galarts <arturgalbinean@gmail.com>
2024-07-11 14:59:27 +0300

    commit

--- food.py
+++ food.py
@@ -1,43 +1,52 @@
 
-
-# import streamlit as st
-from search_and_filter import search_by_name, filter_by_time, filter_by_category, filter_by_ingredients
-from recipe_manager import RecipeManager
 import streamlit as st
 from PIL import Image
-
-
-
-
-
-
+from search_and_filter import search_by_name, filter_by_time, filter_by_category, filter_by_ingredients
+from recipe_manager import RecipeManager
 
 def main():
     st.title(":green[_Rețete Culinare_]")
     page_img = Image.open('food_ingredients.jpg')
     st.image(page_img)
-    
 
     with st.sidebar:
         st.header("Adaugă o rețetă nouă")
+        
         title = st.text_input("Titlul Rețetei")
         ingredients = st.text_area("Ingredientele", height=100)
         steps = st.text_area(":green[Pașii de preparare]", height=100)
         time = st.number_input(
-            "Timp de preparare (minute)", min_value=1, step=1)
+            "Timp de preparare (minute)", min_value=0, step=5)
         category = st.selectbox("Categorie", options=[
                                 "Desert", "Fel principal", "Aperitiv", "Salată"])
+        
+        image = st.file_uploader("Încarcă o imagine pentru rețetă", type=['jpg', 'jpeg', 'png'])
+        
         if st.button("Adaugă Rețeta"):
             try:
                 ingredients_list = [
                     ing.strip() for ing in ingredients.split('\n') if ing.strip()]
-                recipe_manager.add_recipe({
+                
+                if image is not None:
+                    image = Image.open(image)
+                    recipe_manager.add_recipe({
+                        "title": title,
+                        "ingredients": ingredients_list,
+                        "steps": steps,
+                        "time": time,
+                        "category": category
+                    }, image)
+                else:
+                    recipe_manager.add_recipe({
                     "title": title,
-                    "ingredients": [ing.strip() for ing in ingredients.split(',')],
+                    "ingredients": ingredients_list,
                     "steps": steps,
                     "time": time,
                     "category": category
-                })
+                }, None)
+
+
+
                 st.success("Rețetă adăugată cu succes!")
             except ValueError as e:
                 st.error(str(e))
@@ -49,7 +58,7 @@ def main():
     filter_category = st.selectbox("Filtrează după categorie", [
                                    "Toate", "Desert", "Fel principal", "Aperitiv", "Salată"])
     available_ingredients = st.text_input(
-        "Ingrediente disponibile (separate prin virgulă)")
+        "Rețetă după ingrediente disponibile (separați prin virgulă)")
     recipes = recipe_manager.get_recipes()
     if search_term:
         recipes = search_by_name(recipes, search_term)
@@ -69,7 +78,11 @@ def main():
         st.write("Pașii de preparare:", recipe['steps'])
         st.write("Timp de preparare:", str(recipe['time']) + " minute")
         st.write("Categorie:", recipe['category'])
-
+        
+        
+        if 'image_path' in recipe:
+            recipe_image = Image.open(recipe['image_path'])
+            st.image(recipe_image, caption='Imagine rețetă', use_column_width=True)
 
 if __name__ == "__main__":
     recipe_manager = RecipeManager()
--- recipe_manager.py
+++ recipe_manager.py
@@ -1,18 +1,25 @@
 import json
-
+import os
 
 class RecipeManager:
-    def __init__(self, filename='recipes.json'):
+    def __init__(self, filename='recipes.json',image_folder='recipe_images'):
         self.filename = filename
+        self.image_folder = image_folder
+        
+        if not os.path.exists(self.image_folder):
+            os.makedirs(self.image_folder)
         try:
             with open(self.filename, 'r') as file:
                 self.recipes = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             self.recipes = []
 
-    def add_recipe(self, recipe):
+    def add_recipe(self, recipe,image):
         required_fields = ["title", "ingredients", "steps", "time", "category"]
         if all(field in recipe for field in required_fields):
+            image_filename = os.path.join(self.image_folder, recipe['title'].replace(' ', '_') + '.jpg')
+            image.save(image_filename)
+            recipe['image_path'] = image_filename
             self.recipes.append(recipe)
             with open(self.filename, 'w') as file:
                 json.dump(self.recipes, file, indent=4)
